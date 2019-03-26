04a5e17db7f00f95c177cb8dad925065e215eab1
Joe Barnes and michael Larragueta <michael@>
2019-03-26 13:09:32 -0700

    Add flip side of cards

--- js/script.js
+++ js/script.js
@@ -1,9 +1,71 @@
 $(function(){
-  $(".btn").click(function(){
+  $(".btn-popup").click(function(){
     $(".popup-container").toggle("btn-popup1");
   });
   $(".popup-text").click(function(){
     $(".popup-container").toggle("btn-popup1");
   });
+
+  $(".btn-popup2").click(function(){
+    $(".popup-container2").toggle("btn-popup1");
+  });
+
+  $(".popup-text2").click(function(){
+    $(".popup-container2").toggle("btn-popup1");
+  });
+
+
+  $(".btn-popup3").click(function(){
+    $(".popup-container3").toggle("btn-popup1");
+  });
+
+  $(".popup-text3").click(function(){
+    $(".popup-container3").toggle("btn-popup1");
+  });
+
+
+  $(".btn-popup4").click(function(){
+    $(".popup-container4").toggle("btn-popup1");
+  });
+
+  $(".popup-text4").click(function(){
+    $(".popup-container4").toggle("btn-popup1");
+  });
+
+  $(".btn-popup5").click(function(){
+    $(".popup-container5").toggle("btn-popup1");
+  });
+
+  $(".popup-text5").click(function(){
+    $(".popup-container5").toggle("btn-popup1");
+  });
+
+
+  $(".btn-popup6").click(function(){
+    $(".popup-container6").toggle("btn-popup1");
+  });
+
+  $(".popup-text6").click(function(){
+    $(".popup-container6").toggle("btn-popup1");
+  });
+
+
+  $(".btn-popup7").click(function(){
+    $(".popup-container7").toggle("btn-popup1");
+  });
+
+  $(".popup-text7").click(function(){
+    $(".popup-container7").toggle("btn-popup1");
+  });
+
+
+  $(".btn-popup8").click(function(){
+    $(".popup-container8").toggle("btn-popup1");
+  });
+
+  $(".popup-text8").click(function(){
+    $(".popup-container8").toggle("btn-popup1");
+  });5
+
 
 });
